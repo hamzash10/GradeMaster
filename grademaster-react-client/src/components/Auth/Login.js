bbb8df646b13c489c667a82c329cfe6872be190c
hamzash10 <hamzashaheen.HAS@gmail.com>
2024-10-24 18:15:02 +0300

    added a basic frontend design to the login page

--- grademaster-react-client/src/components/Auth/Login.js
+++ grademaster-react-client/src/components/Auth/Login.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { login } from '../../services/ApiCalls/auth';
 import { useNavigate } from 'react-router-dom';
 
+import { Modal, Button, Form, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -21,23 +23,49 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-            />
-            <button type="submit">Login</button>
-        </form>
+        <Container>
+            <Modal show centered backdrop={false} keyboard={false}>
+
+                <Modal.Header className="d-flex justify-content-center">
+                    <Modal.Title className="text-center mb-4">Login</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="formEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter your email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+
+                <Modal.Footer className="d-flex flex-column justify-content-center">
+                    <Button type="submit" className="btn  btn-primary btn-block w-100">
+                        <h5>Login</h5>
+                    </Button>
+                    <p className="mt-2">
+                        Don't have an account? <Link to="/register">Register</Link>
+                    </p>
+                </Modal.Footer>
+            </Modal>
+
+        </Container>
     );
 };
 
